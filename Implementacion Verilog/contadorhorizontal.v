5348abba167cf914b9a4e184c99d157f7cf064c7
luiso1530 <merayo05@gmail.com>
2016-08-11 16:09:18 -0600

    ampliar a 1600

--- Implementacion Verilog/contadorhorizontal.v
+++ Implementacion Verilog/contadorhorizontal.v
@@ -18,24 +18,29 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+/*
+	LOGBOOK:
+	11-Ago-2016: Fue ampliado el rango de operacion a 11 bits, con tope de 1600
+*/
 module contadorhorizontal(Clk, reset, cuenta);
 	//entradas
 	input Clk, reset;
 	//salida
 	output cuenta;
-	reg [9:0] cuenta;
+	reg [10:0] cuenta;
 	always @ (posedge Clk)
 	begin
 		// Condición de reset
 		if (reset)
 		begin
-			cuenta <= 10'd0;
+			cuenta <= 11'd0;
 		end
 		else
 		// Contar de forma ascendente
 		begin
 			//final de la cuenta
-			if (cuenta == 800) cuenta <= 0;
+			if (cuenta == 1599) cuenta <= 0;
 			//aumento de la cuenta
 			else cuenta <= cuenta + 1;
 		end
